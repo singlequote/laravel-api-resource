b564e76739e7361bfd907cdb33bf6199c706b5ed
Wim Pruiksma <wim@quotec.nl>
2024-03-20 17:33:30 +0100

    Updates

--- src/config/config.php
+++ src/config/config.php
@@ -3,19 +3,12 @@
 return [
 
     'namespaces' => [
-        //        'controllers' => "App\Http\Controllers",
-        'controllers' => "Modules\{module}\App\Http\Controllers\{module}",
-        //        'models' => "App\Models",
-        'models' => "Modules\{module}\App\Models",
-        //        'actions' => "App\Actions",
-        'actions' => "Modules\Api\App\Features\{module}",
-        //        'requests' => "App\Http\Requests",
-        'requests' => "Modules\Api\App\Http\Requests\{module}",
-        //        'resources' => "App\Http\Resources",
-        'resources' => "Modules\Api\App\Http\Resources\{module}",
-
-        //        'translations' => "",
-        'translations' => '{module}::',
+        'controllers' => "App\Http\Controllers",
+        'models' => "App\Models",
+        'actions' => "App\Actions",
+        'requests' => "App\Http\Requests",
+        'resources' => "App\Http\Resources",
+        'translations' => '',
     ],
 
     'exclude' => [
