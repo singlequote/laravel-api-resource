c6a72b65aaf7a169e027b4c24ced3ba8b2c42327
Wimurk <wimurk@users.noreply.github.com>
2024-05-23 14:59:31 +0200

    Update ApiRequestService.php

--- src/Service/ApiRequestService.php
+++ src/Service/ApiRequestService.php
@@ -1,9 +1,13 @@
 <?php
-
 namespace SingleQuote\LaravelApiResource\Service;
 
 class ApiRequestService
 {
+
+    /**
+     * @param string $model
+     * @return array
+     */
     public static function defaults(string $model): array
     {
         return [
@@ -13,55 +17,52 @@ class ApiRequestService
             'search' => 'nullable|array',
             'search.fields' => 'nullable|array',
             'search.query' => 'nullable|string|min:2|max:191',
-
             // Set where
             'where' => 'nullable|array',
             'where.*' => 'required|string',
-
             // Set whereIn
             'whereIn' => 'nullable|array',
             'whereIn.*' => 'required|array',
-
             // Set whereNotIn
             'whereNotIn' => 'nullable|array',
             'whereNotIn.*' => 'required|array',
-
-            // Set whereNotIn
-            'whereNotNull' => 'nullable|array',
-            'whereNotNull.*' => 'required|array',
-
+            // Set whereNotNull
+            'whereNotNull' => 'nullable|string',
             // Set WhereHas
             'has' => 'nullable|array',
-            'has.*' => 'required|string|in:'.self::getRelations($model),
-
+            'has.*' => 'required|string|in:' . self::getRelations($model),
             // Set Where Relation
             'whereRelation' => 'nullable|array',
             'whereRelation.*' => 'required|array',
-
             // Set With relations
             'with' => 'nullable|array',
-            'with.*' => 'required|string|in:'.self::getRelations($model),
-
+            'with.*' => 'required|string|in:' . self::getRelations($model),
             // Set select on columns
             'select' => 'nullable|array',
-            'select.*' => 'required|string|in:'.self::getFillable($model),
-
+            'select.*' => 'required|string|in:' . self::getFillable($model),
             // Set the order
-            'orderBy' => 'nullable|string|in:updated_at,created_at,'.self::getFillable($model),
-            'orderByDesc' => 'nullable|string|in:updated_at,created_at,'.self::getFillable($model),
+            'orderBy' => 'nullable|string|in:updated_at,created_at,' . self::getFillable($model),
+            'orderByDesc' => 'nullable|string|in:updated_at,created_at,' . self::getFillable($model),
         ];
     }
 
+    /**
+     * @param string $model
+     * @return array
+     */
     public static function attributes(string $model): array
     {
         return [
-
         ];
     }
 
+    /**
+     * @param string $model
+     * @return string
+     */
     public static function getRelations(string $model): string
     {
-        if (! class_exists($model)) {
+        if (!class_exists($model)) {
             return '';
         }
 
@@ -72,9 +73,13 @@ class ApiRequestService
         return collect($apiIncluded)->implode(',');
     }
 
+    /**
+     * @param string $model
+     * @return string
+     */
     public static function getFillable(string $model): string
     {
-        if (! class_exists($model)) {
+        if (!class_exists($model)) {
             return '';
         }
 
@@ -86,7 +91,7 @@ class ApiRequestService
         $hidden = (new $model())->getHidden();
 
         return collect($fillables)->filter(function ($fill) use ($hidden) {
-            return ! in_array($fill, $hidden);
-        })->implode(',');
+                return !in_array($fill, $hidden);
+            })->implode(',');
     }
 }
