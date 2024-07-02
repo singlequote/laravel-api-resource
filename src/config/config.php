56efbd3d18b16c09c7e695e813fad4d880bb46da
Wimurk <wimurk@users.noreply.github.com>
2024-07-02 15:53:20 +0200

    Update config.php

--- src/config/config.php
+++ src/config/config.php
@@ -1,7 +1,13 @@
 <?php
 
 return [
-
+    
+    /*
+    |--------------------------------------------------------------------------
+    | namespaces
+    |--------------------------------------------------------------------------
+    | Set the namespace directories to store generated files
+   */
     'namespaces' => [
         'controllers' => "App\Http\Controllers",
         'models' => "App\Models",
@@ -10,14 +16,38 @@ return [
         'resources' => "App\Http\Resources",
         'translations' => '',
     ],
-
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Exclude columns
+    |--------------------------------------------------------------------------
+    | This will exlude columns used to render the request or resource files. For example, a password should not be visible in either resources or requests
+    | For example a password should not be visible in either resources or requests
+   */
     'exclude' => [
         'resources' => [],
         'requests' => [],
     ],
 
     'api' => [
+        /*
+        |--------------------------------------------------------------------------
+        | Limit
+        |--------------------------------------------------------------------------
+        | Maximum results per page the api can return
+       */
         'limit' => 1000,
+        
+        'relations' => [
+            /*
+            |--------------------------------------------------------------------------
+            | Relation depth
+            |--------------------------------------------------------------------------
+            | This determines how many relationships the API can use. For example, a depth of 2 can read the relationships of a relationship.
+            | a depth of 2 can read the relationships of a relationship.
+           */
+            'depth' => 1,
+        ]
     ],
 
 ];
