de3c87013fde81767b71afddbb42e5cf789fde5e
Wimurk <wimurk@users.noreply.github.com>
2024-07-03 09:46:53 +0200

    Update ApiRequestService.php

--- src/Service/ApiRequestService.php
+++ src/Service/ApiRequestService.php
@@ -7,16 +7,19 @@ use Throwable;
 use function collect;
 use function config;
 
-class ApiRequestService {
+class ApiRequestService
+{
 
     /**
      * @param string $model
      * @return array
      */
-    public static function defaults(string $model): array {
+    public static function defaults(string $model): array
+    {
         return [
             // Set limit per page
-            'limit' => 'nullable|int|max:' . config('laravel-api-resource.api.limit', 1000) . '|min:1',
+            'limit' => 'nullable|int|max:'.config('laravel-api-resource.api.limit',
+                1000).'|min:1',
             // Search on results
             'search' => 'nullable|array',
             'search.fields' => 'nullable|array',
@@ -34,20 +37,20 @@ class ApiRequestService {
             'whereNotNull' => 'nullable|string',
             // Set WhereHas
             'has' => 'nullable|array',
-            'has.*' => 'required|string|in:' . self::getRelations($model),
+            'has.*' => 'required|string|in:'.self::getRelations($model),
             // Set Where Relation
             'whereRelation' => 'nullable|array',
             'whereRelation.*' => 'required|array',
             'whereRelation.*.*' => ['required', new MixedRule()],
             // Set With relations
             'with' => 'nullable|array',
-            'with.*' => 'required|string|in:' . self::getRelations($model),
+            'with.*' => 'required|string|in:'.self::getRelations($model),
             // Set select on columns
             'select' => 'nullable|array',
-            'select.*' => 'required|string|in:' . self::getFillable($model),
+            'select.*' => 'required|string|in:'.self::getFillable($model),
             // Set the order
-            'orderBy' => 'nullable|string|in:updated_at,created_at,' . self::getFillable($model),
-            'orderByDesc' => 'nullable|string|in:updated_at,created_at,' . self::getFillable($model),
+            'orderBy' => 'nullable|string|in:updated_at,created_at,'.self::getFillable($model),
+            'orderByDesc' => 'nullable|string|in:updated_at,created_at,'.self::getFillable($model),
         ];
     }
 
@@ -55,7 +58,8 @@ class ApiRequestService {
      * @param string $model
      * @return array
      */
-    public static function attributes(string $model): array {
+    public static function attributes(string $model): array
+    {
         return [
         ];
     }
@@ -64,9 +68,10 @@ class ApiRequestService {
      * @param string $modelClass
      * @return string
      */
-    public static function getRelations(string $modelClass): string {
+    public static function getRelations(string $modelClass): string
+    {
         try {
-            $model = (new $modelClass);
+            $model     = (new $modelClass);
             $relations = $model->definedRelations();
         } catch (Throwable $ex) {
             return '';
@@ -77,13 +82,13 @@ class ApiRequestService {
         foreach ($relations as $relation) {
 
             $additionalRelations[] = $relation;
-            
-            try{
+
+            try {
                 $relationModel = $model->$relation()->getModel();
             } catch (\Throwable $ex) {
                 continue;
             }
-            
+
 
             if (isset($relationModel->apiRelations)) {
                 $additionalRelations = [
@@ -102,7 +107,8 @@ class ApiRequestService {
      * @param string $model
      * @return string
      */
-    public static function getFillable(string $model): string {
+    public static function getFillable(string $model): string
+    {
         if (!class_exists($model)) {
             return '';
         }
@@ -115,7 +121,7 @@ class ApiRequestService {
         $hidden = (new $model())->getHidden();
 
         return collect($fillables)->filter(function ($fill) use ($hidden) {
-            return !in_array($fill, $hidden);
-        })->implode(',');
+                return !in_array($fill, $hidden);
+            })->implode(',');
     }
 }
